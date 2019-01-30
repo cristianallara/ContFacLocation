ed264f7f80c0eb1cb05f22561e426b47f97454d3
cristianallara <cristial@andrew.cmu.edu>
2019-01-30 11:06:00 -0500

    Final version of the code for the first submission to CACE

--- plot_performance_curve.py
+++ plot_performance_curve.py
@@ -4,24 +4,24 @@ import matplotlib.pyplot as plt
 
 x = [4.53, 16.6, 125.9, 404.2, 466.1, 2413, 3377, 3601, 3601]
 y = [0, 1, 2, 3, 4, 5, 6, 7, 10]
-plt.step(x, y, label='Acc. Bilevel Decomp.', linestyle='-')
+plt.step(x, y, label='Acc. Bilevel Decomp.', linestyle='-', marker='^', linewidth=3.0, markersize=7)
 
 x = [7.9, 16.5, 1230.1, 1911, 2689.5, 3601, 3601]
 y = [0, 1, 2, 3, 4, 5, 10]
-plt.step(x, y, label='Bilevel Decomp.', linestyle='--', dashes=(5,2))
+plt.step(x, y, label='Bilevel Decomp.', linestyle=':',  marker='s', linewidth=4.0, markersize=6)
 
 x = [5.55, 35, 279.9, 2149, 3601, 3601]
 y = [0, 1, 2, 3, 4, 10]
-plt.step(x, y, label='BARON', linestyle='-.')
+plt.step(x, y, label='BARON', linestyle='-.', marker='x', markersize=6)
 
 x = [7.96, 42.25, 3601, 3601]
 y = [0, 1, 2, 10]
-plt.step(x, y, label='SCIP', linestyle='--', dashes=(3,3,2,2))
+plt.step(x, y, label='SCIP', linestyle=':', marker='o', linewidth=2.0, markersize=6)
 
 x = [96.19, 147.79, 3601, 3601]
 y = [0, 1, 2, 10]
-plt.step(x, y, label='ANTIGONE', linestyle=':', color='k')
-# plt.xscale('log')
+plt.step(x, y, label='ANTIGONE', linestyle='--', marker='*', markersize=7, color='k')
+plt.xscale('log')
 
 plt.legend()
 plt.ylabel("Number of instances solved")
